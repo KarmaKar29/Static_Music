a0db973f531a56317c5e71ff11d1bfd1e85846fc
.Karma <kartik.19.yay@gmail.com>
2025-08-26 23:30:01 +0530

    upload

---
+++ config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+  TOKEN: "",
+  language: "en",
+  ownerID: ["1004206704994566164", ""], 
+  mongodbUri : "mongodb+srv://kartik:Kartik@static.j3wujef.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "",
+  spotifyClientSecret : "",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/Ut9g9JwVHA",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "Karma",
+      password: "karma19",
+      host: "5.39.63.207",
+      port:  8262,
+      secure: false
+    }
+  ]
+}
