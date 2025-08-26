279641bb3f7557bcdafbf692a15ab095a3d476b2
.Karma <kartik.19.yay@gmail.com>
2025-08-27 00:11:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "Music", 
+  activityName: "Pink Floyd", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/Ut9g9JwVHA",
   embedTimeout: 5, 
